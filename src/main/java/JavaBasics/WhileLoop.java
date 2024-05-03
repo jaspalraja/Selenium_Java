1b78936c755762b3b54e5786ac1e403cb6d8d796
jaspa <jaspa@LAPTOP-E4LC4M5L>
2024-05-03 07:54:14 +0530

    While loop

---
+++ src/main/java/JavaBasics/WhileLoop.java
@@ -0,0 +1,16 @@
+package JavaBasics;
+
+public class WhileLoop {
+
+	public static void main(String[] args) {
+		
+		int i = 1;
+		
+		while(i<=23)
+		{
+			System.out.println(i);
+			i++;
+		}
+	}
+
+}
